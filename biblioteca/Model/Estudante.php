f0f52c74f2cdf1a506c4623c6c43e3160d9b9180
mw-bl <bezerram533@gmail.com>
2024-08-16 13:47:41 -0300

    :sparkles: feat: Adding new attributes

--- biblioteca/Model/Estudante.php
+++ biblioteca/Model/Estudante.php
@@ -1,13 +1,15 @@
 <?php
 namespace Model;
 
-class Autor {
+class Estudante {
     private $id;
     private $nome;
+    private $matricula;
 
-    public function __construct($id, $nome) {
+    public function __construct($id, $nome, $matricula) {
         $this->id = $id;
         $this->nome = $nome;
+        $this->matricula = $matricula;
     }
 
     public function getId() {
@@ -21,5 +23,11 @@ class Autor {
         return $this->nome = $nome;
     }
 
+    public function getMatricula() {
+        return $this->matricula;
+    }
+    public function setMatricula($matricula) {
+        return $this->matricula = $matricula;
+    }
 }
 ?>
