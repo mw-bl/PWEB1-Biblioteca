214150824e5633f3a6325c0b2d8f3785efebb81e
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2024-09-09 14:01:53 -0300

    Update Emprestimo.php

--- biblioteca/Model/Emprestimo.php
+++ biblioteca/Model/Emprestimo.php
@@ -10,16 +10,19 @@ class Emprestimo {
         $this->livros = [];
     }
 
+    // Função para emprestar um livro
     public function emprestarLivro($livro, $estudante) {
         $repo = new EmprestimoRepository();
         return $repo->registrarEmprestimo($livro->getId(), $estudante->getIdEstudante());
     }
 
+    // Função para devolver um livro
     public function devolverLivro($livro, $estudante) {
         $repo = new EmprestimoRepository();
         return $repo->registrarDevolucao($livro->getId(), $estudante->getIdEstudante());
     }
 
+    // Função para listar os livros emprestados
     public function livrosEmprestados($estudante) {
         $repo = new EmprestimoRepository();
         return $repo->listarLivrosEmprestados($estudante->getIdEstudante());
