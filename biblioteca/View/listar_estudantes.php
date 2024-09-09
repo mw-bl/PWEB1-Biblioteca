dd6dc139603aef3a6fc5d0fe51f42bb9b923499b
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2024-09-09 14:26:26 -0300

    Update listar_estudantes.php

--- biblioteca/View/listar_estudantes.php
+++ biblioteca/View/listar_estudantes.php
@@ -6,6 +6,7 @@ error_reporting(E_ALL);
 include_once '../Controller/EstudanteController.php';
 use Controller\EstudanteController;
 
+// Chama a função listarEstudantes() do controller para fazer a listagem dos estudantes
 $controller = new EstudanteController();
 $estudantes = $controller->listarEstudantes();
 ?>
