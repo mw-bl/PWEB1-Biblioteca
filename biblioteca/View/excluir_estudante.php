a21574d87af4773542f09ffa2a6c56a1d5be9730
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2024-09-09 14:23:38 -0300

    Update excluir_estudante.php

--- biblioteca/View/excluir_estudante.php
+++ biblioteca/View/excluir_estudante.php
@@ -6,8 +6,10 @@ error_reporting(E_ALL);
 include_once '../Controller/EstudanteController.php';
 use Controller\EstudanteController;
 
+// Realiza um GET para pegar o Id do estudante
 $id = $_GET['id'];
 
+// Chama a função excluirAutor() do Controller para realizar a exclusao do estudante pelo ID chamado
 $controller = new EstudanteController();
 $controller->excluirEstudante($id);
 header('Location: listar_estudantes.php');
