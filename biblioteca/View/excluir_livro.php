e60ac43413fcbf62471ca61303e1404ed8309747
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2024-09-09 14:25:05 -0300

    Update excluir_livro.php

--- biblioteca/View/excluir_livro.php
+++ biblioteca/View/excluir_livro.php
@@ -6,8 +6,10 @@ error_reporting(E_ALL);
 include_once '../Controller/LivroController.php';
 use Controller\LivroController;
 
+// Realiza um GET para pegar o Id do livro
 $id = $_GET['id'];
 
+// Chama a função excluirLivro() do Controller para realizar a exclusao do livro pelo ID chamado
 $controller = new LivroController();
 $controller->excluirLivro($id);
 header('Location: listar_livros.php');
