ad2d9559fb675f6c41bc0ae90ebeb7d352e5e446
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2024-09-09 14:22:28 -0300

    Update excluir_autor.php

--- biblioteca/View/excluir_autor.php
+++ biblioteca/View/excluir_autor.php
@@ -6,11 +6,11 @@ error_reporting(E_ALL);
 include_once '../Controller/AutorController.php';
 use Controller\AutorController;
 
+// Realiza um GET para pegar o Id do autor
 $id = $_GET['id'];
 
-// Realiza um GET para pegar o Id do autor
+// Chama a função excluirAutor() do Controller para realizar a exclusao do autor
 $controller = new AutorController();
-// Chama a função excluirAutor() para realizar a exclusao do autor
 $controller->excluirAutor($id);
 header('Location: listar_autores.php');
 exit;
